8fefbaa93c525f59ac5e4c0ea0fe8ae5cdf24a9f
Choudhary Awais <chawaisdev92@gmail.com>
2025-11-20 18:00:56 +0500

    changes

--- database/migrations/2025_11_20_081009_create_adons_table.php
+++ database/migrations/2025_11_20_081009_create_adons_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('addons', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('menu_id');
             $table->string('name');
             $table->decimal('price', 8, 2)->default(0);
             $table->timestamps();
